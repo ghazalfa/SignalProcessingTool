d91cb59d67a6d9a8aac608d40cbf67f921b94793
ghazalfa <83470392+ghazalfa@users.noreply.github.com>
2023-07-24 14:14:20 -0700

    peak_finder completed

--- src/mods/peak_finder.rs
+++ src/mods/peak_finder.rs
@@ -1,119 +1,132 @@
 use crate::RawData;
+use find_peaks::{self, Peak};
 
-pub struct PeakFinder{
 
-}
 
-impl PeakFinder{
+pub struct Peak_Finder{}
+
+impl Peak_Finder{
 
     pub fn new() -> Self{
-        PeakFinder { }
-    }
-
-    //returns the maximum value inside of the array
-    //if array is empty, returns zero - check with sadra to see if he wants zero or the None in Option<f32>
-
-    pub fn local_min(data: RawData) -> f32 {
-        match data{
-            RawData::FloatVec(data) =>{
-
-                if data.is_empty() {
-                    return 0.0; // Return None if the vector is empty
-                }
-            
-                let mut min_value = data[0];
-            
-                for &value in data.iter() {
-                    if value < min_value {
-                        min_value = value;
-                    }
-                }
-            
-                min_value
-            
-
-                }
-
-            RawData::IntVec(data) =>{
-                
-                let data: Vec<f32> = data.iter().map(|&x| x as f32).collect();
-        
-                if data.is_empty() {
-                    return 0.0; // Return None if the vector is empty
-                }
-                
-                let mut min_value = data[0];
-                
-                for &value in data.iter() {
-                    if value < min_value {
-                            min_value = value;
-                    }
-                }
-                
-                min_value
-        
-        
-            }
-
-        }
-
-
-            }
-
-
-        
-
-
-        //returns the maximum value inside of the array
-        //if array is empty, returns zero - check with sadra to see if he wants zero or the None in Option<f32>
-
-        pub fn local_max(data: RawData) -> f32 {
-match data{
-            RawData::FloatVec(data) =>{
-
-                if data.is_empty() {
-                    return 0.0; // Return None if the vector is empty
-                }
-            
-                let mut min_value = data[0];
-            
-                for &value in data.iter() {
-                    if value < min_value {
-                        min_value = value;
-                    }
-                }
-            
-                min_value
-
-                }
-
-            RawData::IntVec(data) =>{
-                
-                let data: Vec<f32> = data.iter().map(|&x| x as f32).collect();
-        
-                if data.is_empty() {
-                    return 0.0; // Return None if the vector is empty
-                }
-                
-                let mut min_value = data[0];
-                
-                for &value in data.iter() {
-                    if value < min_value {
-                            min_value = value;
-                    }
-                }
-                
-                min_value
-        
-        
-            }
-
-        }
-    
-    
+        Peak_Finder{}
     }
 
 
+    //finds peaks in in the vector in the RawData struct, takes in no paramaters regarding height, prominence, distance, etc
+    pub fn run_peakfinder(&mut self, input: &RawData) -> Vec<Peak<f32>>{
+
+        //matches RawData struct to get to input
+        match input{
+
+            RawData::FloatVec(input) => {
+
+                //calls on Peakfinder to find peaks without any parameters
+                let peakvector: Vec<Peak<f32>> = find_peaks::PeakFinder::new(&input).find_peaks();
+
+                return peakvector
+            }
+
+            RawData::IntVec(input) => {
+
+                //converts the Vec<i32> to Vec<f32>
+                let input: Vec<f32> = input.iter().map(|&x| x as f32).collect();
+
+                //calls on Peakfinder to find peaks without any parameters                
+                let peakvector: Vec<Peak<f32>> = find_peaks::PeakFinder::new(&input).find_peaks();
+
+                return peakvector
+
+            }
+        }
+
+    }
+
+
+    //finds peaks in in the vector in the RawData struct, takes in parameters for minimum height and minimum prominence
+    pub fn run_peakfinder_min_hp(&mut self, input: &RawData, min_height: f32, min_prominence: f32) -> Vec<Peak<f32>>{
+
+        //matches RawData struct to get to input
+        match input{
+
+            RawData::FloatVec(input) => {
+
+                //calls on Peakfinder to find peaks with a certain minimum height and prominence
+                let peakvector: Vec<Peak<f32>> = find_peaks::PeakFinder::new(&input).with_min_height(min_height).with_min_prominence(min_prominence).find_peaks();
+
+                return peakvector
+            }
+
+            RawData::IntVec(input) => {
+
+                //converts the Vec<i32> to Vec<f32>
+                let input: Vec<f32> = input.iter().map(|&x| x as f32).collect();
+
+                //calls on Peakfinder to find peaks with a certain minimum height and prominence
+                let peakvector: Vec<Peak<f32>> = find_peaks::PeakFinder::new(&input).with_min_height(min_height).with_min_prominence(min_prominence).find_peaks();
+
+                return peakvector
+
+            }
+        }
+
+    }
+
+    //finds peaks in in the vector in the RawData struct, takes in parameters for minimum height and prominence, as well as max height and prominence
+    pub fn run_peakfinder_min_hp_max_hp(&mut self, input: &RawData, min_height: f32, min_prominence: f32, max_height: f32, max_prominence: f32) -> Vec<Peak<f32>>{
+        
+        //matches RawData struct to get to input
+        match input{
+
+            RawData::FloatVec(input) => {
+
+                //calls on Peakfinder to find peaks with a certain minimum height and prominence, and max height and prominence
+                let peakvector: Vec<Peak<f32>> = find_peaks::PeakFinder::new(&input).with_min_height(min_height).with_min_prominence(min_prominence).with_max_height(max_height).with_max_prominence(max_prominence).find_peaks();
+
+                return peakvector
+            }
+
+            RawData::IntVec(input) => {
+
+                //converts the Vec<i32> to Vec<f32>
+                let input: Vec<f32> = input.iter().map(|&x| x as f32).collect();
+
+                //calls on Peakfinder to find peaks with a certain minimum height and prominence, and max height and prominence
+                let peakvector: Vec<Peak<f32>> = find_peaks::PeakFinder::new(&input).with_min_height(min_height).with_min_prominence(min_prominence).with_max_height(max_height).with_max_prominence(max_prominence).find_peaks();
+
+                return peakvector
+
+            }
+        }
+
+    }
+
+    //finds peaks in in the vector in the RawData struct, takes in parameters for minimum height, distance and plateau size
+    pub fn run_peakfinder_min_hdp(&mut self, input: &RawData, min_height: f32, min_distance: f32, min_plateausize: f32) -> Vec<Peak<f32>>{
+       
+        //matches RawData struct to get to input
+        match input{
+
+            RawData::FloatVec(input) => {
+
+                //calls on Peakfinder to find peaks with a certain minimum height, distance, and plateau size
+                let peakvector: Vec<Peak<f32>> = find_peaks::PeakFinder::new(&input).with_min_height(min_height).with_min_distance(min_distance as usize).with_min_plateau_size(min_plateausize as usize).find_peaks();
+
+                return peakvector
+            }
+
+            RawData::IntVec(input) => {
+
+                //converts the Vec<i32> to Vec<f32>
+                let input: Vec<f32> = input.iter().map(|&x| x as f32).collect();
+                
+                //calls on Peakfinder to find peaks with a certain minimum height, distance, and plateau size
+                let peakvector: Vec<Peak<f32>> = find_peaks::PeakFinder::new(&input).with_min_height(min_height).with_min_distance(min_distance as usize).with_min_plateau_size(min_plateausize as usize).find_peaks();
+
+                return peakvector
+
+            }
+        }
+
+    }
 }
-
 
